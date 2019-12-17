0fbe6e93a5c3b009c835e3cfb3173b745fbe1803
abdelhamed alomar <abdelhamed.alomar.1996@gmail.com>
2019-12-17 12:08:20 +0100

    johan

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,7 +1,23 @@
 // The following line makes sure your styles are included in the project. Don't remove this.
 import '../styles/main.scss';
 // Import any additional modules you want to include below \/
-
+console.log('conecd')
 
 // \/ All of your javascript should go here \/
 
+var modal = document.querySelector('.modal')
+var close = document.querySelector('.close')
+
+var promise1 = new Promise(function(resolve, reject) {
+    setTimeout(function() {
+      resolve('');
+    }, 60000);
+  });
+  promise1.then(function() {
+    modal.style.display = 'block';
+
+  });
+
+  close.addEventListener("click",event=>{
+    modal.style.display= "none"
+  })
